139e8fbd8282a65f82b0097b962ddcfd3d2add8f
nanhaiyipianxin <105554030+nanhaiyipianxin@users.noreply.github.com>
2023-04-09 21:41:44 +0800

    Create main.c

---
+++ 多文件编程51/main.c
@@ -0,0 +1,14 @@
+#include "Delay.h"
+#include "neixie.h"
+#include <REGX52.H>
+void main(){
+    while(1){
+     nixie(1,2);
+     //Delay(5);
+    nixie(2,0);
+     //Delay(5);
+    nixie(3,2);
+     nixie(4,3);
+     //Delay(5);
+}
+}
